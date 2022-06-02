96ad117f6dd96a16e56f8fd3e3ffec46730bfc2d
BullsnakeWilson <wilson.holiday@lyonsexpress.com>
2022-06-02 12:14:05 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'newproject622022',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
